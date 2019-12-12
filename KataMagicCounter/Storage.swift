08340e14788582ebe4a2d2927f01b8c7c55ef978
Fernando <markussen145@gmail.com>
2019-12-12 11:48:28 +0100

    First commit.

---
+++ KataMagicCounter/Storage.swift
@@ -0,0 +1,20 @@
+//
+//  Storage.swift
+//  KataMagicCounter
+//
+//  Created by Fernando Gil Otero on 12/12/2019.
+//  Copyright © 2019 Fernando Gil Otero. All rights reserved.
+//
+
+import Foundation
+
+class Storage{
+    
+    func save(value: Int) {
+        
+    }
+    
+    func get() -> Int? {
+        return 1
+    }
+}
