4abadf266302bd9299dacc939ab83354f7e20545
Fernando <markussen145@gmail.com>
2019-12-12 11:40:32 +0100

    Initial Commit

---
+++ KataMagicCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KataMagicCounter
+//
+//  Created by Fernando Gil Otero on 12/12/2019.
+//  Copyright © 2019 Fernando Gil Otero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
